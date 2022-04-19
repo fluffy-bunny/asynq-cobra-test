6166e0dac9ad3c23ad3fbc0c4bc040dca3a336e7
Herb Stahl <ghstahl@gmail.com>
2022-04-19 11:08:49 -0700

    Initial commit

---
+++ cmd/cli/cmd/about/about.go
@@ -0,0 +1,34 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package about
+
+import (
+	"fmt"
+
+	"cobra_starter/cmd/cli/shared"
+	"cobra_starter/internal/cobra_utils"
+
+	"github.com/spf13/cobra"
+)
+
+// aboutCmd represents the about command
+var aboutCmd = &cobra.Command{
+	Use:               "about",
+	PersistentPreRunE: cobra_utils.ParentPersistentPreRunE,
+	Short:             "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("version: %s\n", shared.Version)
+	},
+}
+
+func InitCommand(parent *cobra.Command) {
+	parent.AddCommand(aboutCmd)
+}
